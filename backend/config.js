6d311a6b722c1083b5a740d860742e54db6ac427
hemritifss <111984262+hemritifss@users.noreply.github.com>
2024-07-06 19:10:59 +0100

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -3,5 +3,5 @@ require('dotenv').config();
 module.exports = {
   PORT: process.env.PORT || 5000,
   MONGO_URI: process.env.MONGO_URI,
-  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_SECRET: process.env.Tnet_dR0n3_secret,
 };
